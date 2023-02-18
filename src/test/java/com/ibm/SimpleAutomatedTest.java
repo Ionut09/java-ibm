0f1c4c1be50f9a71170d8f436590500ea318ed89
Ionut Spalatelu <ionut.spalatelu@vdx.tv>
2023-02-18 17:16:46 +0200

    selenium

--- src/test/java/com/ibm/SimpleAutomatedTest.java
+++ src/test/java/com/ibm/SimpleAutomatedTest.java
@@ -1,25 +1,31 @@
 package com.ibm;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SimpleAutomatedTest {
 	
-	private ChromeOptions options = new ChromeOptions()
-		                                .addArguments("--no-sandbox");
-	private WebDriver driver = new ChromeDriver(options);
+	private SafariOptions options = new SafariOptions();
+	private WebDriver driver = new SafariDriver(options);
+	
+	@BeforeClass
+	public void setup(){
+		System.setProperty("webdriver.safari.driver", "/Users/ionut/workspace/trainings/java-ibm/src/test/resources/chromedriver");
+	}
 	
 	@Test
 	public void test() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver",
-			"/Users/ionut/workspace/trainings/java-ibm/src/test/resources/chromedriver");
 		
 		driver.navigate()
 		      .to("https://www.calculator.net/mortgage-payoff-calculator.html");
 		
 		Thread.sleep(2000);
 	}
+	
+	
+	
 	
 }
